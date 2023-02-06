a1483c00060d9537e163031d3156116316f78b93
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2023-02-06 20:30:54 -0300

    cosas

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import HelloWorld from './components/HelloWorld.vue'
 import DarkModeToggle from './components/DarkModeToggle.vue'
 import Sidebar from './components/Sidebar.vue'
 import Header from './components/Header.vue';
+import WeatherWidget from './components/Weather/WeatherWidget.vue';
 </script>
 
 <template>
   <div id="page-container" class="sidebar-o side-trans-enabled ">
     <Sidebar />
     <Header />
+    <WeatherWidget/>
     
   </div>
 
